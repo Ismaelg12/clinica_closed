6c62b393f517b3c20e0423e423cf5901b075aae8
Ismael <is_contabil@outlook.com>
2021-05-17 01:26:50 -0300

    base de dadsos

--- sistema_fisioterapia/settings.py
+++ sistema_fisioterapia/settings.py
@@ -91,7 +91,7 @@ WSGI_APPLICATION = 'sistema_fisioterapia.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
-"""
+
 DATABASES = {
     'default': {
         'ENGINE':'django.db.backends.mysql',
@@ -112,7 +112,7 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
+"""
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
 
